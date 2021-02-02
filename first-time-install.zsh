a1108e49ea0d2ebe2353b71a196a360de82220c2
Adam2Marsh <adam.marsh@rocketmail.com>
2021-02-02 22:53:42 +0000

    Update first-time-install.zsh
    Removing brew

--- first-time-install.zsh
+++ first-time-install.zsh
@@ -22,30 +22,26 @@ if [ -z "${ZDOTDIR}" ]; then
     echo "export ZDOTDIR=${MY_ZDOTDIR}" | sudo tee -a /etc/zshenv >> /dev/null
 fi
 
-if [ ! -f /usr/local/bin/brew ]; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+# if [ ! -f /usr/local/bin/brew ]; then
+#     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# fi
 
-echo "Updating brew and forumulae."
-brew update && brew upgrade
+# echo "Updating brew and forumulae."
+# brew update && brew upgrade
 
-brew install git
+# brew install git
 
-if [ ! -d ${MY_ZDOTDIR} ]; then
-    echo "Cloning osx-env to ${MY_ZDOTDIR}"
-    local repo_dir
-    if [[ "${DOTDIR_REPO}" =~ "http.*" ]]; then
-        repo_dir=`echo ${DOTDIR_REPO} | sed  "s+^.*//\(.*\)\.git$+${HOME}/projects/src/\1+"`
-    else
-        repo_dir=`echo ${DOTDIR_REPO} | sed  "s+^git@\(.*\)\:\(.*\)\.git$+${HOME}/projects/src/\1/\2+"`
-    fi
-    git -c http.sslVerify=false clone --recurse-submodules -j8 ${DOTDIR_REPO} ${repo_dir} -b ${DOTDIR_BRANCH:-master}
-    ln -s ${repo_dir} ${MY_ZDOTDIR}
-fi
+# if [ ! -d ${MY_ZDOTDIR} ]; then
+#     echo "Cloning osx-env to ${MY_ZDOTDIR}"
+#     local repo_dir
+#     if [[ "${DOTDIR_REPO}" =~ "http.*" ]]; then
+#         repo_dir=`echo ${DOTDIR_REPO} | sed  "s+^.*//\(.*\)\.git$+${HOME}/projects/src/\1+"`
+#     else
+#         repo_dir=`echo ${DOTDIR_REPO} | sed  "s+^git@\(.*\)\:\(.*\)\.git$+${HOME}/projects/src/\1/\2+"`
+#     fi
+#     git -c http.sslVerify=false clone --recurse-submodules -j8 ${DOTDIR_REPO} ${repo_dir} -b ${DOTDIR_BRANCH:-m1}
+#     ln -s ${repo_dir} ${MY_ZDOTDIR}
+# fi
 
-brew tap Homebrew/bundle
-brew bundle --verbose --file=${MY_ZDOTDIR}/Brewfile
-
-#scarey stuff
-defaults write com.googlecode.iterm2 PrefsCustomFolder "$MY_ZDOTDIR"
-defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder "1"
+# brew tap Homebrew/bundle
+# brew bundle --verbose --file=${MY_ZDOTDIR}/Brewfile
